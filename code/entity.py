2cb005ea55e4cd09a995b1d902ca6a7cd8113be5
Suedna Souza <suednasouzaa@gmail.com>
2025-08-28 15:26:23 -0300

    Fixing imports in Level.py file

--- code/entity.py
+++ code/entity.py
@@ -9,10 +9,10 @@ import pygame.image
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load(f'./asset/' + name + '.png')
-        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.surf = pygame.image.load(f'./asset/' + name + '.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=position[0], top=position[1]
+)
         self.speed = 0
-
 
     @abstractmethod
     def move(self, ):
