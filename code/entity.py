77fdb6e722dbe485a6a1a90e28473da8e91b513c
Suedna Souza <suednasouzaa@gmail.com>
2025-08-29 10:35:45 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/entity.py
+++ code/entity.py
@@ -5,14 +5,16 @@ from abc import ABC, abstractmethod
 
 import pygame.image
 
+from code.Const import ENTITY_HEALTH
+
 
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
         self.surf = pygame.image.load(f'./asset/' + name + '.png').convert_alpha()
-        self.rect = self.surf.get_rect(left=position[0], top=position[1]
-)
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
+        self.health = ENTITY_HEALTH[self.name]
 
     @abstractmethod
     def move(self, ):
